e3765b8c79e679591dfe285168042f7e5049ca7a
Pedro Pires <33130521+PaperButt@users.noreply.github.com>
2018-09-03 05:11:27 +0100

    Added a lot of shit

--- javascript/section.js
+++ javascript/section.js
@@ -3,16 +3,6 @@ $(document).ready(function() {
     gridTop = windowHeight * 0.2,
     gridBottom = windowHeight * 0.8;
   $(window).on('scroll', function() {
-    $('.teslovsnote .section_name').each(function() {
-      var thisTop = $(this).offset().top - $(window).scrollTop();
-
-      if (thisTop > gridTop && (thisTop + $(this).height()) < gridBottom) {
-            $(this).css('filter', 'grayscale(0)');
-      } else {
-            $(this).css('filter', 'grayscale(1)');
-
-      }
-    });
     $('.devblog_section .section_name').each(function() {
       var thisTop = $(this).offset().top - $(window).scrollTop();
 
