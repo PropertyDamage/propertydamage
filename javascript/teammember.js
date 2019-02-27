81660b113adb8773e775269b61b38c79b8bad5c6
Pedro <plpires99@gmail.com>
2019-02-27 21:34:58 +0000

    Just don't look until it's done
    trying to make this website viable

--- javascript/teammember.js
+++
@@ -1,9 +0,0 @@
-function displayMemberRole(element) {
-  console.log("meme");
-  $(element).next().css("opacity","1");
-}
-
-function removeMemberRole(element) {
-  console.log("meme");
-  $(element).next().css("opacity","0");
-}
